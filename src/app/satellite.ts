b1e42041509a33b4dc34b35ddf7e6d61cebb54ec
Arpita Vatsa <arrpita@gmail.com>
2020-08-02 22:19:39 -0500

    intial checkin

---
+++ src/app/satellite.ts
@@ -0,0 +1,28 @@
+export class Satellite {
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, 
+        orbitType: string, operational: boolean){
+            
+        this.name = name;
+        this.orbitType = orbitType;
+        this.type=type;
+        this.launchDate=launchDate;
+        this.operational=operational;
+        }
+        shouldShowWarning():boolean{
+            let debrisWarning = this.type;
+            if(debrisWarning.toLocaleLowerCase() ==="Space Debris"){
+                return true;
+            }
+            else {
+                return false;
+            };
+        }
+    
+}
